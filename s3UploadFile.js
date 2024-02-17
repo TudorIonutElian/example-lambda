8731fb284bb5a35d0a770027a0f62ddf99fa3183
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-17 17:46:31 +0200

    Change lambda logic

--- s3UploadFile.js
+++ s3UploadFile.js
@@ -4,29 +4,20 @@ const path = require('path');
 
 const uploadToFirstS3 = async (generateFileContent) => {
     const s3 = new S3({
-        /* Local rules for AWS credentials
-        credentials: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        },
-        */
         region: 'eu-central-1',
         signatureVersion: 'v4',
       });
     
-    const uploadToFirstS3 = (passThroughStream) => (new Promise((resolve, reject) => {
     const uploadParams = {
-        Bucket: 'cloudwatch-mock-lambda-bucket',
-        Key: Date.now().toString() + '.txt',
-        Body: generateFileContent.toString(),
+      Bucket: 'cloudwatch-mock-lambda-bucket',
+      Key: Date.now().toString() + '.txt',
+      Body: generateFileContent.toString(),
     };
-    s3.putObject(uploadParams, (err) => {
+    
+    return s3.putObject(uploadParams, (err) => {
         if (err) reject(err);
         resolve(true);
     });
-    }));
-
-    return await uploadToFirstS3(fs.createReadStream(path.join(__dirname, 'request.txt')));
 }
 
 module.exports = { uploadToFirstS3 };
