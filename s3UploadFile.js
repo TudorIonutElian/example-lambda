033f5ab046d3c1143e4aecc048efc4c626206533
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-17 17:22:32 +0200

    Prepare lambda without creds

--- s3UploadFile.js
+++ s3UploadFile.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 const uploadToFirstS3 = async (generateFileContent) => {
     const s3 = new S3({
+        /* Local rules for AWS credentials
         credentials: {
           accessKeyId: process.env.AWS_ACCESS_KEY_ID,
           secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         },
+        */
         region: 'eu-central-1',
         signatureVersion: 'v4',
       });
